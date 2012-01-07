0d74159aa846251543b5f78510cffcadbdad85b7
Leslie T <leslie_tanengsy@yahoo.com>
2012-01-07 13:30:19 +0800

    Edit flash message after transfer

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -62,7 +62,7 @@ class TransactionsController < ApplicationController
         User.add_balance(user_to,amount)
       end
       
-      flash[:notice] = "You have successfully transferred #{amount} to #{current_user.surname} at #{phone}"
+      flash[:notice] = "You have successfully transferred #{amount} to #{user_to.surname} at #{phone}"
       redirect_to balance_url
     end
   end
