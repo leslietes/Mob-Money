f19d59835b886541ec7b516e23463eabe9d63b23
Leslie T <leslie_tanengsy@yahoo.com>
2012-01-09 17:15:57 +0800

    Fix session store error

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Mobmoney::Application.config.session_store :cookie_store, key: '_mobmoney_session'
+Mobmoney::Application.config.session_store :cookie_store, :key => '_mobmoney_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
